8b58553e212d4884468572f98e76c195c554c937
Latoya Matai <latoyamatai0615@gmail.com>
2026-05-18 16:13:23 +0200

    Added RESPONSIVE JS

---
+++ sentinelops/apps/frontend/src/responsive.js
@@ -0,0 +1,9 @@
+// responsive.js
+
+export const getDeviceType = () => {
+  const width = window.innerWidth;
+
+  if (width <= 768) return "mobile";
+  if (width <= 1024) return "tablet";
+  return "desktop";
+};
